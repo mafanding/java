e9139ca22097c30d29b7f05df88b9d9a0a62a78b
mfdboy <mfdboy@163.com>
2016-06-12 06:57:25 +0800

    test

---
+++ NamedPoint.java
@@ -0,0 +1,17 @@
+import java.awt.Point;
+
+class NamedPoint extends Point {
+	String name;
+
+	NamedPoint(int x, int y,String name){
+		super(x,y);
+		this.name=name;
+	}
+
+	public static void main(String[] args) {
+		NamedPoint np=new NamedPoint(5,5,"hahahahah");
+		System.out.println("x is "+np.x);
+		System.out.println("y is "+np.y);
+		System.out.println("Name is "+np.name);
+	}
+}
---
+++ Printer.java
@@ -0,0 +1,18 @@
+class Printer {
+	int x=0;
+	int y=1;
+
+	void printMe(){
+		System.out.println("x is "+x+", y is "+y);
+		System.out.println("I am instance of the class "+this.getClass().getName());
+	}
+}
+
+class SubPrinter extends Printer {
+	int z=3;
+
+	public static void main(String[] args) {
+		SubPrinter obj=new SubPrinter();
+		obj.printMe();
+	}
+}
